4a16544bdc1d3358e111429379a9b177ed40a383
n1k0din <nik726@gmail.com>
2021-09-10 14:54:08 +0200

    Add cli params, uvloop and autoreload

--- entry.py
+++ entry.py
@@ -1,5 +1,6 @@
-import asyncio
 import aiohttp
+import argparse
+import asyncio
 
 try:
     import uvloop
@@ -7,11 +8,23 @@ try:
 except ImportError:
     print('uvloop is not available')
 
-
 from pixel_hunter import create_app
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', help='Hostname', default='0.0.0.0')
+parser.add_argument('--port', help='Port', default=8080)
+parser.add_argument('--reload', action='store_true', help='Reload parameters')
+
+args = parser.parse_args()
+
 app = create_app()
 
+if args.reload:
+    print('Start with code reload')
+    import aioreloader
+    aioreloader.start()
+
+
 if __name__ == '__main__':
-    aiohttp.web.run_app(app)
+    aiohttp.web.run_app(app, host=args.host, port=args.port)
